e5384c12df73dd52495a1bfb5a224296896b34c1
Jairo <jaiiro.cereceda@gmail.com>
2026-04-03 13:30:19 +0200

    feat: add first route

---
+++ src/app/shared/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'main-layout',
+  templateUrl: './main-layout.component.html',
+  imports: [RouterOutlet],
+})
+export class MainLayoutComponent {}
