d9b9433be404071dc4ebf48367b09794a7a9a2fb
Jairo <jaiiro.cereceda@gmail.com>
2026-04-06 20:11:42 +0200

    feat: create auth login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,24 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
 import { ProfileComponent } from './features/profile/profile.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './features/auth/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
     children: [{ path: 'profile', component: ProfileComponent }],
+  },
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+    ],
   },
 ];
