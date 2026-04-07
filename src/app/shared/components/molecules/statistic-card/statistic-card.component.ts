e080de8adb81d27f9933195ecdf1b3bdd705b365
Jairo <jaiiro.cereceda@gmail.com>
2026-04-07 21:53:31 +0200

    feat: add username in statistic cards for followed people

--- src/app/shared/components/molecules/statistic-card/statistic-card.component.ts
+++ src/app/shared/components/molecules/statistic-card/statistic-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, input } from '@angular/core';
 import { StatisticCardComponentInterface } from './statistic-card.interface';
 import { TitleComponent } from '../../atoms/title/title.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-statistic-card',
   templateUrl: './statistic-card.component.html',
-  imports: [TitleComponent],
+  imports: [TitleComponent, RouterLink],
 })
 export class StatisticCardComponent {
   statisticCard = input<StatisticCardComponentInterface>();
--- src/app/shared/components/molecules/statistic-card/statistic-card.interface.ts
+++ src/app/shared/components/molecules/statistic-card/statistic-card.interface.ts
@@ -2,6 +2,8 @@ import { TitleComponentInterface } from '../../atoms/title/title.interface';
 
 export interface StatisticCardComponentInterface {
   name: TitleComponentInterface;
+  userName: string;
   quantity: number;
   isLoggedAccount: boolean;
+  isNotProfileView?: boolean;
 }
